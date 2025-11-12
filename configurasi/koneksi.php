642bffcd48985edd85b24783842863228032e4a5
326167Dian <ahmadnurdiansyah81@gmail.com>
2025-11-12 14:42:35 +0700

    setting lagi koneksi hosting

--- configurasi/koneksi.php
+++ configurasi/koneksi.php
@@ -1,26 +1,26 @@
 <?php
-// Date_Default_timezone_set('Asia/jakarta');
-// $server = "localhost";
-// $user = "u877780297_ina"; 
-// $password = "7390091979Dian&&"; 
-// $database = "u877780297_inafarma";
-// set_time_limit(1800);
-
-// include_once "conn.php";
-// ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
-// mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
-// // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
-// $db = mysqli_connect('localhost', 'u877780297_ina', '7390091979Dian&&', 'u877780297_inafarma');
-
 Date_Default_timezone_set('Asia/jakarta');
 $server = "localhost";
-$user = "root"; 
-$password = ""; 
-$database = "inafarma";
+$user = "u877780297_ina"; 
+$password = "7390091979Dian&&"; 
+$database = "u877780297_inafarma";
 set_time_limit(1800);
 
 include_once "conn.php";
 ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
 mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
 // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
-$db = mysqli_connect('localhost', 'root', '', 'inafarma');
+$db = mysqli_connect('localhost', 'u877780297_ina', '7390091979Dian&&', 'u877780297_inafarma');
+
+// Date_Default_timezone_set('Asia/jakarta');
+// $server = "localhost";
+// $user = "root"; 
+// $password = ""; 
+// $database = "inafarma";
+// set_time_limit(1800);
+
+// include_once "conn.php";
+// ($GLOBALS["___mysqli_ston"] = mysqli_connect($server, $user, $password)) or die("Koneksi gagal");
+// mysqli_select_db($GLOBALS["___mysqli_ston"], $database) or die("Database tidak ditemukan");
+// // $koneksi = new mysqli($server, $user, $password) or die("Koneksi gagal");
+// $db = mysqli_connect('localhost', 'root', '', 'inafarma');
